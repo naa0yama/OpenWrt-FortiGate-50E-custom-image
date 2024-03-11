150ea9420accfd5474947b7bd4e926c5b2d17e39
naa0yama <9667078+naa0yama@users.noreply.github.com>
2024-03-11 20:09:30 +0900

    fix config

--- config/imagebuilder/files/etc/uci-defaults/99-custom.sh
+++ config/imagebuilder/files/etc/uci-defaults/99-custom.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 # DO NOT prefix 'uci'
 
-cat << "EOF" > /etc/uci-defaults/99-custom
-uci -q batch << EOI
-set firewall.@defaults[0].flow_offloading='1'
-set irqbalance.irqbalance.enabled='1'
-set system.@system[0].timezone='JST-9'
-
-EOI
+uci -q batch <<-EOF >/dev/null
+  set firewall.@defaults[0].flow_offloading='1'
+  set irqbalance.irqbalance.enabled='1'
+  set system.@system[0].timezone='JST-9'
+  commit
 EOF
+
+exit 0
